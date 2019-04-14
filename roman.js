c512daf875683c723245bf80d08a0c1ed5fa6731
Samantha Carlin <samcarlin@samanthas-mbp.home>
2019-04-13 21:09:40 -0400

    should convert 1 through 10

--- roman.js
+++ roman.js
@@ -1,5 +1,38 @@
-function convert() {
-  return 'I'
+function convert(arabic) {
+  if (arabic == 1) {
+    return 'I'
+  } 
+  if (arabic == 2) {
+    return 'II'
+  }
+  if (arabic == 3){
+    return 'III'
+  }
+
+  if (arabic === 4) {
+    return 'IV'
+  }
+  
+  if (arabic == 5) {
+    return 'V'
+  }
+  
+  if (arabic == 6) {
+    return 'VI'
+  }
+
+  if (arabic == 7) {
+    return 'VII'
+  }
+
+  if (arabic == 8) {
+    return 'VIII'
+  }
+if (arabic == 9) {
+  return 'IX'
+}
+return 'X'
+
 }
 
 module.exports = { convert }
--- roman.test.js
+++ roman.test.js
@@ -5,4 +5,49 @@ describe('roman', () => {
     let result = roman.convert(1)
     expect(result).toBe('I')
   })
+
+  test('returns II for 2', () => {
+    let result = roman.convert(2)
+    expect(result).toBe('II')
+  })
+
+  test('returns III for 3', () => {
+    let result = roman.convert(3)
+    expect(result).toBe('III')
+  })
+
+  test('returns IV for 4', () => {
+    let result = roman.convert(4)
+    expect(result).toBe('IV')
+  })
+
+  test('returns V for 5', () => {
+    let result = roman.convert(5)
+    expect(result).toBe('V')
+  })
+
+  test('returns VI for 6', () => {
+    let result = roman.convert(6)
+    expect(result).toBe('VI')
+  })
+
+  test('returns VII for 7', () => {
+    let result = roman.convert(7)
+    expect(result).toBe('VII')
+  })
+
+  test('returns VIII for 8', () => {
+    let result = roman.convert(8)
+    expect(result).toBe('VIII')
+  })
+
+  test('returns IX for 9', () => {
+    let result = roman.convert(9)
+    expect(result).toBe('IX')
+  })
+
+  test('returns X for 10', () => {
+    let result = roman.convert(10)
+    expect(result).toBe('X')
+  })
 })
