ac3b80ddf6365086af3fe7e0fae9230fb1f19fb8
Samantha Carlin <samcarlin@samanthas-mbp.home>
2019-04-13 21:20:02 -0400

    refactor 1 through 10

--- roman.js
+++ roman.js
@@ -1,38 +1,12 @@
+const singleDigits = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+
 function convert(arabic) {
-  if (arabic == 1) {
-    return 'I'
-  } 
-  if (arabic == 2) {
-    return 'II'
-  }
-  if (arabic == 3){
-    return 'III'
+  if (arabic == 10) {
+    return 'X'
   }
 
-  if (arabic === 4) {
-    return 'IV'
-  }
-  
-  if (arabic == 5) {
-    return 'V'
-  }
-  
-  if (arabic == 6) {
-    return 'VI'
-  }
-
-  if (arabic == 7) {
-    return 'VII'
-  }
-
-  if (arabic == 8) {
-    return 'VIII'
-  }
-if (arabic == 9) {
-  return 'IX'
-}
-return 'X'
-
+  const index = arabic - 1
+  return singleDigits[index]
 }
 
 module.exports = { convert }
